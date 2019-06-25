ddc41090cf1eb4e87dbbe45893274e54b24361f1
RenzoLiu0209 <34439930+RenzoLiu0209@users.noreply.github.com>
2019-06-25 13:10:29 +0800

    Add files via upload

---
+++ src/main/java/com/renzo/ordersystem/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.renzo.ordersystem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OrdersystemApplication.class);
+	}
+
+}
